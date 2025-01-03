ca997c8d594817d11a38321895914bab3b4de1eb
王鑫哲 <User_wang_178@163.com>
2025-01-03 14:30:06 +0800

    #task ListUtil 完善

--- lib_utils/src/main/java/com/example/lib_utils/ListUtil.java
+++ lib_utils/src/main/java/com/example/lib_utils/ListUtil.java
@@ -2,6 +2,7 @@ package com.example.lib_utils;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 /**
  * Created by 王鑫哲 on 2022/7/19 10:25 上午
@@ -10,11 +11,18 @@ import java.util.Arrays;
  */
 public class ListUtil {
 
-    @SafeVarargs
-    public static <T> ArrayList create(T... data) {
-        if (data == null || data.length <= 0) {
+    public static <T> List<T> create(T... data) {
+        if (data == null || data.length == 0) {
             return new ArrayList<T>();
         }
-        return new ArrayList(Arrays.asList(data));
+        return Arrays.asList(data);
+    }
+
+    public static <T> boolean isEmpty(List<T> list) {
+        return list == null || list.isEmpty();
+    }
+
+    public static <T> boolean isNotEmpty(List<T> list) {
+        return list != null && !list.isEmpty();
     }
 }
